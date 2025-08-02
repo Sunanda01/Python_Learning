df839595f8dbaa343bcb8f09bfc6c8dde94018f1
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-08-03 00:37:09 +0530

    DS in Python done

---
+++ DS/Extra/random_module.py
@@ -0,0 +1,16 @@
+import random
+
+num=random.randint(10,20)
+print(num)
+
+ran_range=random.randrange(10,20)
+print(ran_range)
+
+li=[1,8,9,2,3]
+random.shuffle(li)
+print(li)
+
+a=random.uniform(1,3)
+print(a)
+
+print(random.random())
