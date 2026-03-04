b726f3a12fb2902fcd1524ad8d920f933f3e57d1
Sunanda01 <sunandasadhukhan1@gmail.com>
2026-03-04 22:59:35 +0530

    Adding intro an dmodels of langchain

---
+++ 2.0Langchain/1.0Intro&Models.py
@@ -0,0 +1,212 @@
+# Components of langchain
+
+1. Models 
+2. Prompt 
+3. Chains
+4. Indexes
+5. Memory
+6. Agents
+
+# Packages requirments.txt
+# LangChain Core
+langchain
+langchain-core
+
+# OpenAI Integration
+langchain-openai
+openai
+
+# Anthropic Integration
+langchain-anthropic
+
+# Google Gemini (PaLM) Integration
+langchain-google-genai
+google-generativeai
+
+# Hugging Face Integration
+langchain-huggingface
+transformers
+huggingface-hub
+
+# Environment Variable Management
+python-dotenv
+
+# Machine Learning Utilities
+numpy
+scikit-learn
+
+
+
+# -----------------------------------------------------------------------
+
+# Model is divided in to Language model and Embeded Model -> Languaeg Model further divided in to LLM and Chatbot
+
+# 1. LLM useing Open AI : Recommend to use chat model instead LLM
+
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model='gpt-3.5-turbo-instruct')
+
+result = llm.invoke("What is the capital of India")
+
+print(result)
+
+# Chat Models
+# 1. Open AI 
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
+
+result = model.invoke("Write a 5 line poem on cricket")
+
+print(result.content)
+
+
+# Anthropic
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatAnthropic(model='claude-3-5-sonnet-20241022')
+
+result = model.invoke('What is the capital of India')
+
+print(result.content)
+
+
+# Gemini
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+result = model.invoke('What is the capital of India')
+
+print(result.content)
+
+# Opensource models
+# Hugging face api
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
+
+
+# Hugging face  local
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+
+# os.environ['HF_HOME'] = 'D:/huggingface_cache' -> neede if locally thing not want to store in c drive
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
+
+
+# Embedding models
+# Single open ai query 
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
+
+result = embedding.embed_query("Delhi is the capital of India")
+
+print(str(result))
+
+# Open ai docs or multiple query 
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
+
+documents = [
+    "Delhi is the capital of India",
+    "Kolkata is the capital of West Bengal",
+    "Paris is the capital of France"
+]
+
+result = embedding.embed_documents(documents)
+
+print(str(result))
+
+# Using Hugging face embedding (sentence transformer)
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+documents = [
+    "Delhi is the capital of India",
+    "Kolkata is the capital of West Bengal",
+    "Paris is the capital of France"
+]
+
+vector = embedding.embed_documents(documents)
+
+print(str(vector))
+
+# Symmentic search 
+
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=300)
+
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = 'tell me about bumrah'
+
+doc_embeddings = embedding.embed_documents(documents)
+query_embedding = embedding.embed_query(query)
+
+scores = cosine_similarity([query_embedding], doc_embeddings)[0]
+
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is:", score)
